5217b20f70d07fd91a88ce2464288d61eeb5f1b7
Bilger Yahov <bilger@studyportals.com>
2017-07-08 19:52:32 +0200

    Adapt to new system. Remove old modular code.

--- CMS-Modules/Modules/IndexPageStaticContent/index_page_static_content.js
+++ CMS-Modules/Modules/IndexPageStaticContent/index_page_static_content.js
@@ -12,10 +12,6 @@ const IndexPageStaticContent = (function(){
 
     const Logic = {
 
-        _templatePath: './CMS-Modules/CMS-Modules/Modules/IndexPageStaticContent/index_page_static_content.html',
-        _placeholderName: 'IndexPageStaticContentPlaceholder',
-        _template: null,
-
         /**
          * Initializes the main functionality.
          *
@@ -24,25 +20,7 @@ const IndexPageStaticContent = (function(){
 
         init(){
 
-            const $self = this;
-            $self.renderTemplate();
-        },
 
-        /**
-         * Renders the template.
-         *
-         * @return void
-         */
-
-        renderTemplate(){
-
-            const $self = this;
-
-            $self._template = new Template(
-                $self._templatePath, $self._placeholderName, {}
-            );
-
-            $self._template.displayMain();
         }
     };
 
