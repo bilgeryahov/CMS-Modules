9869942822640f7fb48a6f2106dce84aca1c05be
Bilger Yahov <bilger@studyportals.com>
2017-07-08 19:19:51 +0200

    Adapt to new system. Remove old modular code.

--- CMS-Modules/Modules/Header/header.js
+++ CMS-Modules/Modules/Header/header.js
@@ -12,10 +12,6 @@ const Header = (function(){
 
     const Logic = {
 
-        _templatePath: './CMS-Modules/CMS-Modules/Modules/Header/header.html',
-        _placeholderName: 'HeaderPlaceholder',
-        _template: null,
-
         /**
          * Initializes the main functionality.
          *
@@ -24,25 +20,7 @@ const Header = (function(){
 
         init(){
 
-            const $self = this;
-            $self.renderTemplate();
-        },
 
-        /**
-         * Renders the template.
-         *
-         * @return void
-         */
-
-        renderTemplate(){
-
-            const $self = this;
-
-            $self._template = new Template(
-                $self._templatePath, $self._placeholderName, {}
-            );
-
-            $self._template.displayMain();
         }
     };
 
