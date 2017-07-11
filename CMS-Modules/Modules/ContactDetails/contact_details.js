d05831056590ff9238b88671bac6b43606ea347d
Bilger Yahov <bilger@studyportals.com>
2017-07-11 16:53:48 +0200

    Switch to new templating logic (engine).

--- CMS-Modules/Modules/ContactDetails/contact_details.js
+++ CMS-Modules/Modules/ContactDetails/contact_details.js
@@ -12,10 +12,6 @@ const ContactDetails = (function(){
 
     const Logic = {
 
-        _templatePath: './CMS-Modules/CMS-Modules/Modules/ContactDetails/contact_details.html',
-        _placeholderName: 'ContactDetailsPlaceholder',
-        _template: null,
-
         /**
          * Initializes the main functionality.
          *
@@ -24,25 +20,7 @@ const ContactDetails = (function(){
 
         init(){
 
-            const $self = this;
-            $self.renderTemplate();
-        },
 
-        /**
-         * Renders the template.
-         *
-         * @return void
-         */
-
-        renderTemplate(){
-
-            const $self = this;
-
-            $self._template = new Template(
-                $self._templatePath, $self._placeholderName, {}
-            );
-
-            $self._template.displayMain();
         }
     };
 
