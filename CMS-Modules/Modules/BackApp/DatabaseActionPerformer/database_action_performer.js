8531cf71abcead13be6b7c2eb964c16a7b5d7811
Bilger Yahov <bilger@studyportals.com>
2017-10-29 18:00:43 +0100

    Initialize code on the new module controller.

---
+++ CMS-Modules/Modules/BackApp/DatabaseActionPerformer/database_action_performer.js
@@ -0,0 +1,30 @@
+/**
+ * @file database_action_performer.js
+ *
+ * DatabaseActionPerformer module controller.
+ *
+ * @author Bilger Yahov <bayahov1@gmail.com>
+ * @version 1.0.0
+ * @copyright © 2017 Bilger Yahov, all rights reserved.
+ */
+
+const DatabaseActionPerformer = (function(){
+
+	const Logic = {
+
+
+	};
+
+	return{
+
+		init(){
+
+			Logic.init();
+		}
+	}
+})();
+
+document.addEvent('domready', function () {
+
+	DatabaseActionPerformer.init();
+});
