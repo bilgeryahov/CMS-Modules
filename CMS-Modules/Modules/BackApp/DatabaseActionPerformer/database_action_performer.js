e71d83bbca992dbc5a720787cf647cea71004899
Bilger Yahov <bilger@studyportals.com>
2017-10-29 18:08:29 +0100

    Change alert with console info.

--- CMS-Modules/Modules/BackApp/DatabaseActionPerformer/database_action_performer.js
+++ CMS-Modules/Modules/BackApp/DatabaseActionPerformer/database_action_performer.js
@@ -14,7 +14,7 @@ const DatabaseActionPerformer = (function(){
 
 		init(){
 
-			alert('DatabaseActionPerformer.init(): Initialized!');
+			console.info('DatabaseActionPerformer.init(): Initialized!');
 		}
 	};
 
