04a7359045b94584a90649a5d03ba5f911f37f59
Bilger Yahov <bilger@studyportals.com>
2017-10-29 18:23:24 +0100

    Further implement the JS controller.

--- CMS-Modules/Modules/BackApp/DatabaseActionPerformer/database_action_performer.js
+++ CMS-Modules/Modules/BackApp/DatabaseActionPerformer/database_action_performer.js
@@ -12,9 +12,143 @@ const DatabaseActionPerformer = (function(){
 
 	const Logic = {
 
+		_getButton: null,
+		_postButton: null,
+		_putButton: null,
+		_deleteButton: null,
+		_multiLocationUpdateButton: null,
+
+		/**
+		 * Initializes the main functionality.
+		 */
+
 		init(){
 
-			console.info('DatabaseActionPerformer.init(): Initialized!');
+			const $self = this;
+
+			if(!$self.getDomElements()){
+
+				console.error('DatabaseActionPerformer.init(): Missing DOM element');
+				return;
+			}
+
+			$self.attachDomElementEvents();
+		},
+
+		/**
+		 * Fetches the DOM elements.
+		 *
+		 * @return {boolean}
+		 */
+
+		getDomElements(){
+
+			const $self = this;
+
+			$self._getButton = $('GetButton');
+			$self._putButton = $('PutButton');
+			$self._postButton = $('PostButton');
+			$self._deleteButton = $('DeleteButton');
+			$self._multiLocationUpdateButton = $('MultiLocationUpdateButton');
+
+			return (
+				$self._getButton !== null &&
+					$self._putButton !== null  &&
+					$self._postButton !== null &&
+					$self._deleteButton !== null &&
+					$self._multiLocationUpdateButton !== null
+			);
+		},
+
+		/**
+		 * Attaches the corresponding click event to each button.
+		 *
+		 * @return void
+		 */
+
+		attachDomElementEvents(){
+
+			const $self = this;
+
+			$self._getButton.addEvent('click', function () {
+
+				$self.doGet();
+			});
+
+			$self._putButton.addEvent('click', function () {
+
+				$self.doPut();
+			});
+
+			$self._postButton.addEvent('click', function () {
+
+				$self.doPost();
+			});
+
+			$self._deleteButton.addEvent('click', function () {
+
+				$self.doDelete();
+			});
+
+			$self._multiLocationUpdateButton.addEvent('click', function () {
+
+				$self.doMultiLocationUpdate();
+			});
+		},
+
+		/**
+		 * Perform GET request.
+		 *
+		 * @return void
+		 */
+
+		doGet(){
+
+
+		},
+
+		/**
+		 * Perform PUT request.
+		 *
+		 * @return void
+		 */
+
+		doPut(){
+
+
+		},
+
+		/**
+		 * Perform POST request.
+		 *
+		 * @return void
+		 */
+
+		doPost(){
+
+
+		},
+
+		/**
+		 * Perform DELETE request.
+		 *
+		 * @return void
+		 */
+
+		doDelete(){
+
+
+		},
+
+		/**
+		 * Perform Multi Location Update request.
+		 *
+		 * @return void
+		 */
+
+		doMultiLocationUpdate(){
+
+
 		}
 	};
 
